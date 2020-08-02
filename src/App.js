f44adfad408f2f3fd780533bf0df3b5712549fc2
lubiecki <kontakt@lubiecki.pl>
2020-08-02 17:42:07 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+import Header from "./Header";
+
+const Container = styled.div`
+  & {
+    width: 1440px;
+    margin: 10px 0;
+    padding: 20px;
+    min-height: 100vh;
+    box-sizing: border-box;
+  }
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container>
+            <Header/>
+        </Container>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createGlobalStyle} from "styled-components";
+import {normalize} from 'styled-normalize';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;800&display=swap');
+  ${normalize}
+  body {
+    background: #eee;
+    color: #222831;
+    font-family: 'Open Sans', sans-serif;
+    font-size: 14px;
+    display: flex;
+    justify-content: center;
+  }
+`
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <GlobalStyle/>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
